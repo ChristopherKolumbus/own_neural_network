509ed51176d2de3d5ed345756ea95f15a1d956ef
ChristopherKolumbus <33834216+ChristopherKolumbus@users.noreply.github.com>
2018-01-01 13:21:51 +0100

    Added functionality to test neural network with single test entry

--- first_neural_network.py
+++ first_neural_network.py
@@ -1,7 +1,9 @@
 import csv
+import os
 
 import numpy as np
 from scipy import special
+from matplotlib import pyplot as plt
 
 
 class NeuralNetwork:
@@ -9,8 +11,10 @@ class NeuralNetwork:
         self.input_nodes = input_nodes
         self.hidden_nodes = hidden_nodes
         self.output_nodes = output_nodes
-        self.weights_ih = np.random.rand(self.hidden_nodes, self.input_nodes) - 0.5
-        self.weights_ho = np.random.rand(self.output_nodes, self.hidden_nodes) - 0.5
+        self.weights_ih = np.random.normal(0.0, pow(self.hidden_nodes, -0.5), (self.hidden_nodes, self.input_nodes))
+        self.weights_ho = np.random.normal(0.0, pow(self.output_nodes, -0.5), (self.output_nodes, self.hidden_nodes))
+        # self.weights_ih = np.random.rand(self.hidden_nodes, self.input_nodes) - 0.5
+        # self.weights_ho = np.random.rand(self.output_nodes, self.hidden_nodes) - 0.5
         self.lr = learning_rate
 
     def train(self, inputs_list, targets_list):
@@ -38,6 +42,17 @@ class NeuralNetwork:
         return special.expit(x)
 
 
+def visualize_number(record):
+    image_array = np.array(record[1:], dtype=float).reshape((28, 28))
+    plt.imshow(image_array, cmap='Greys', interpolation='None')
+    plt.title(str(record[0]))
+    plt.show()
+
+
+def prepare_inputs(record):
+    return np.array(record[1:], dtype=float) / 255 * 0.99 + 0.01
+
+
 def main():
     # Specify number of input, hidden and output nodes:
     input_nodes = 784
@@ -48,17 +63,28 @@ def main():
     # Create neural network instance:
     neural_network = NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
     # Load mnist training data from CSV file into list:
-    with open(r'..\..\google_drive\python_data\mnist_data\mnist_train_100.csv', 'r') as training_data_file:
+    data_folder = r'..\..\google_drive\python_data\mnist_data'
+    with open(os.path.join(data_folder, 'mnist_train_100.csv'), 'r') as training_data_file:
         training_data_file_reader = csv.reader(training_data_file, delimiter=',')
         training_data_list = list(training_data_file_reader)
     # Train neural network:
     for record in training_data_list:
         # Scale and shift the inputs:
-        inputs = np.array(record[1:], dtype=float) / 255 * 0.99 + 0.01
+        inputs = prepare_inputs(record)
         # Create target output values (all 0.01 expect desired label which is 0.99):
         targets = np.zeros(output_nodes) + 0.01
         targets[int(record[0])] = 0.99
         neural_network.train(inputs, targets)
+    # Load mnist test data from CSV file into list:
+    with open(os.path.join(data_folder, 'mnist_test_10.csv'), 'r') as test_data_file:
+        test_data_file_reader = csv.reader(test_data_file, delimiter=',')
+        test_data_list = list(test_data_file_reader)
+    # Test neural network:
+    record = test_data_list[3]
+    visualize_number(record)
+    output_outputs = neural_network.query(prepare_inputs(record))
+    print(output_outputs)
+
 
 if __name__ == '__main__':
     main()
