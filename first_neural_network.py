0fbdd16229440ea5cd7ac460fb625ed1b80d206c
ChristopherKolumbus <33834216+ChristopherKolumbus@users.noreply.github.com>
2018-01-05 15:04:20 +0100

    Added default values to parameters for NeuralNetwork class __init__

--- first_neural_network.py
+++ first_neural_network.py
@@ -9,7 +9,7 @@ from matplotlib import pyplot as plt
 
 
 class NeuralNetwork:
-    def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
+    def __init__(self, input_nodes=10, hidden_nodes=10, output_nodes=10, learning_rate=.1):
         self.input_nodes = input_nodes
         self.hidden_nodes = hidden_nodes
         self.output_nodes = output_nodes
